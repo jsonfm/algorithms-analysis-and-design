712ec450c7361a5c5965f7760a170aec87eb03df
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-10-22 10:55:13 -0500

    [UPDATE] multiplication school algorithm

--- algorithms/operations.py
+++ algorithms/operations.py
@@ -1,4 +1,4 @@
-def school_multiplication(a, b):
+def school_multiplication(a, b, verbose=False):
     """Multiplication of two integers using the standard multiplication algorithm.
        a
     x  b
@@ -25,9 +25,9 @@ def school_multiplication(a, b):
     a = obtain_digits(a)
     b = obtain_digits(b)
 
-    print(a)
-    print(b)
-    print()
+    # print(a)
+    # print(b)
+    # print()
 
     a = a[::-1]
     b = b[::-1]
@@ -42,28 +42,65 @@ def school_multiplication(a, b):
             r = b[i] * a[j] + next_digit_sum
             r_digits = obtain_digits(r)
 
-            # print(f"{b[i]} x {a[j]} = {r_digits[-1]} , +{next_digit_sum}")
+            # if verbose:
+            # print(f"{b[i]} x {a[j]} = {r_digits[-1]} (+{next_digit_sum})")
             if len(r_digits) > 1:
-                without_last_digit = r_digits[:1]
+                without_last_digit = r_digits[:-1]
                 next_digit_sum = array_to_number(without_last_digit)
 
-            if j != len(a) - 1:
+            if j < len(a) - 1:
                 r = r_digits[-1]
                 row.append(r)
 
-            if j == len(a) - 1:
+            if j >= len(a) - 1:
                 row = append_number_digits(r, row)
 
         row = row[::-1]
 
-        for k in range(len(a) - i):
+        for _ in range(len(a) - i):
             row.insert(0, 0)
 
-        if len(row) < len(a) + len(b) + 1:
-            for k in range(len(a) + len(b) + 1 - len(row)):
-                row.append(0)
-
+        if len(row) <= len(a) + len(b) + 1:
+            for _ in range(len(a) + len(b) + 1 - len(row)):
+                row.insert(0, 0)
+        # print(row)
         rows.append(row)
 
+    result = []
+    next_carry = 0
 
-school_multiplication(5678, 1234)
+    for i in range(len(a) + len(b) + 1):
+        sum_ = 0
+        string = ""
+
+        for index, row in enumerate(rows):
+            row = row[::-1]
+            sum_ += row[i]
+            if index < len(rows) - 1:
+                string += f"{row[i]} + "
+            else:
+                string += f"{row[i]}"
+
+        sum_ += next_carry
+        string += f" = {sum_}"
+        sum_digits = obtain_digits(sum_)
+        last_sum_digit = sum_digits[-1]
+
+        if len(sum_digits) > 1:
+            without_last_digit = sum_digits[:-1]
+            next_carry = array_to_number(without_last_digit)
+        else:
+            next_carry = 0
+
+        string += f" (+{next_carry})"
+        result.append(last_sum_digit)
+
+    result = result[::-1]
+    # print()
+    # print(result)
+    result = array_to_number(result)
+    return result
+
+
+res = school_multiplication(5678, 1234)
+print(res)
